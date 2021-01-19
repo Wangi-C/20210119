4e5f56d727f1338754c1c6ea48fa2929027b1e2f
Wangi-C <65946357+Wangi-C@users.noreply.github.com>
2021-01-19 22:42:33 +0900

    Add files via upload

---
+++ Dictionary.java
@@ -0,0 +1,34 @@
+package Ch5_10;
+
+public class Dictionary extends PairMap{
+	protected String[][] dic;
+	
+	public Dictionary(int n) {
+		// TODO Auto-generated constructor stub
+		dic = new String[n][];
+	}
+	@Override
+	String get(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void put(String key, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	String delete(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
